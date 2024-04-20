ecf1ec2a79b6a9b20dd326147ad7fc94faa4ae49
phonon-py <phn.19890918@gmail.com>
2024-04-20 10:15:46 +0900

    todo app

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import flet as ft
 
 def main(page: ft.Page):
-    t = ft.Text(value="Hello World")
-    # page.controls.append(t)
-    # page.update()
-    # 上2行の処理を短縮
-    page.add(t)
+    def add_clicked(e):
+        page.add(ft.Checkbox(label=new_task.value))
+        new_task.value = ""
+        page.update()
+    new_task = ft.TextField(hint_text="Whats need to be done?")
+
+    page.add(new_task, ft.FloatingActionButton(icon=ft.icons.ADD, on_click=add_clicked))
+
+
 ft.app(target=main)
