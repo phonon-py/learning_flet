e272b84555dafc83ebf8025d4cf7bcde4022492f
phonon-py <phn.19890918@gmail.com>
2024-04-21 10:46:21 +0900

    レイアウトを学んだ(Col)

--- main.py
+++ main.py
@@ -6,8 +6,25 @@ def main(page: ft.Page):
         new_task.value = ""
         page.update()
     new_task = ft.TextField(hint_text="Whats need to be done?")
-
-    page.add(new_task, ft.FloatingActionButton(icon=ft.icons.ADD, on_click=add_clicked))
+    # 以下が0行目の処理
+    tasks_view = ft.Column()
+    # おそらくカラムの設定
+    view=ft.Column(
+        width=600,
+        controls=[
+            # カラムの0行目
+            ft.Row(
+                controls=[
+                    new_task,
+                    ft.FloatingActionButton(icon=ft.icons.ADD, on_click=add_clicked),
+                ],    
+            ),
+            # どこで配置するか？
+            tasks_view
+        ],
+    )
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(view)
 
 
 ft.app(target=main)
